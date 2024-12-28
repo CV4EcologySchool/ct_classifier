13885172d0049c36cf26f85e483f8d33ee04fe24
shir3bar <shir3bar@gmail.com>
2024-12-28 11:21:09 +0000

    updated download script following LilaBC update

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -14,10 +14,12 @@ destFolder=datasets/CaltechCT
 
 mkdir -p $destFolder
 echo "Downloading images..."
-azcopy copy https://lilablobssc.blob.core.windows.net/caltechcameratraps/eccv_18_all_images_sm.tar.gz $destFolder/.
+wget "https://storage.googleapis.com/public-datasets-lila/caltechcameratraps/eccv_18_all_images_sm.tar.gz" --directory-prefix=$destFolder/
+#azcopy copy https://lilablobssc.blob.core.windows.net/caltechcameratraps/eccv_18_all_images_sm.tar.gz $destFolder/.
 
 echo "Downloading metadata..."
-azcopy copy https://lilablobssc.blob.core.windows.net/caltechcameratraps/eccv_18_annotations.tar.gz $destFolder/.
+wget "https://storage.googleapis.com/public-datasets-lila/caltechcameratraps/eccv_18_annotations.tar.gz" --directory-prefix=$destFolder/
+#azcopy copy https://lilablobssc.blob.core.windows.net/caltechcameratraps/eccv_18_annotations.tar.gz $destFolder/.
 
 echo "Unzipping..."
 tar -xvf $destFolder/eccv_18_all_images_sm.tar.gz -C $destFolder/.
